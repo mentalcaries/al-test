eac2ca48fdd7691d58114e45606c2fa0d39388a1
Devin Jaggernauth <neexis@gmail.com>
2024-07-03 17:45:49 -0400

    update homepage

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename='/al-test' >
       <App />
     </BrowserRouter>
   </React.StrictMode>
